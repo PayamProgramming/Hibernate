74a1b4d8648bd7b2707eb7d5ad91ae7397806869
Payam <payamaghaei22@gmail.com>
2021-12-22 20:57:42 +0330

    First

---
+++ src/hibernate/Test.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate;
+
+import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author intel one
+ */
+
+@ComponentScan
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        Member m = (Member) ctx.getBean("type");
+        
+        
+        // Member m = new Member();
+        m.setId(1);
+        System.out.println("Type Youre Name");
+        String name = input.next();
+        m.setName(name);
+        System.out.println("Type Youre Family");
+        String family = input.next();
+        m.setFamily(family);
+        System.out.println("Type Youre Country");
+        String Country = input.next();
+        m.setCountry(Country);
+        
+        
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        
+        session.save(m);
+        tx.commit();
+        session.close();
+        
+        ShowData shd = new ShowData();
+        shd.show();
+        System.out.println(m.toString());
+        
+        
+    }
+    
+}
